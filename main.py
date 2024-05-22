6952997601d56c3bb15bcdc87031b77e191b4fa1
Lexio0o <159916652+Lexio0o@users.noreply.github.com>
2024-05-22 09:34:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import pyxel
 import random
-
+bonhomme_x = 51
+bonhomme_y = 128
+ennemis_liste = []
 pyxel.init(256, 256)
 pyxel.cls(5)
 pyxel.load("3.pyxres")
+
+def bonhomme_deplacement(x, y)
+    if pyxel.btn(pyxel.KEY_DOWN):
+        if y < 256:
+            y = y + 1.5
+    if pyxel.btn(pyxel.KEY_UP):
+        if y > 0:
+            y = y - 1.5
+    return x, y
 
 def update():
 
