fabfaac5eea6f47729f4bfe1462b1a1338ec4886
JebK00 <86977267+JebK00@users.noreply.github.com>
2024-05-22 09:18:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import pyxel
+import random
 
 class App:
     def __init__(self):
-        pyxel.init(160, 120)
+        pyxel.init(256, 256)
         self.x = 0
         pyxel.run(self.update, self.draw)
 
     def update(self):
 
     def draw(self):
-        pyxel.cls(0)
+        pyxel.cls(1)
         pyxel.rect(self.x, 0, 8, 8, 9)
 
 App()
