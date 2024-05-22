3be06e8a183b30f3cd48796d6120d7073c316202
Lexio0o <159916652+Lexio0o@users.noreply.github.com>
2024-05-22 08:52:52 +0200

    Update main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+
+# initialise
+import pyxel
+import random
+
+pyxel.init(256, 256, title="")
+
+
+
+
+
+def update():
+
+
+
+
+def draw():
+
+
+
+
+
+
+
+
+# executer le jeu:
+# Initialisation du plateau
+init_plateau()
+# Exécution de la boucle principale
+pyxel.run(update, draw)
